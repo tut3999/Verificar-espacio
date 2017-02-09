6926a5209a8a4f9311cd08475f1ba7155072606b
tut3999 <omar3999@gmail.com>
2017-02-09 16:11:13 -0600

    Add files via upload

---
+++ script1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+df -H | grep -vE '^Filesysrem|tmpfs|cdrom|udev' | awk '{print $5 " " $1 }' | while read text;
+do
+	echo $text
+	usep=$(echo $text | awk '{ print $1}' | cut -d'%' -f1)
+	partition=$(echo $text | awk '{print $2}')
+
+#tengo que revisar que $usep sea un numeropara que no mande advertencia 
+	echo $usep
+	if [ $usep -ge 80 ]; then
+		echo `sudo notify-send "Advertencia \"$partition\" esta al 90% de su uso"`
+		#echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)"
+	else 
+		if [ $usep -ge 60 ]; then
+			echo "Tienes un 60% en la particion \"$partition\""
+		fi
+	fi
+done
